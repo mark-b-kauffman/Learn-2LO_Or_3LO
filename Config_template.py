02156a61541bc587a9b33b2c845d284766e87a4e
Mark Kauffman <mark.kauffman@blackboard.com>
2024-02-23 11:12:47 -0800

    Added template for Config file. To do: Delete the Config.py from the repo.

---
+++ Config_template.py
@@ -0,0 +1,6 @@
+#Copy to Config.py. Replace < > content with your values. Remove this comment.
+config = {
+    "key" : "<Your REST Sey Here>",
+    "secret" : "<Your REST Secret Here>",
+    "url" : "<Your Learn Server URL Here. (No trailing/)>",
+}
