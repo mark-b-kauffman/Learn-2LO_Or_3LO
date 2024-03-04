a64d74e7779e58e1cb6ae46d6381fdef42af5c2f
markkauffman2000 <markkauffman2000@gmail.com>
2024-03-03 16:39:06 -0800

    almost v1. need to test

--- Config_template.py
+++ Config_template.py
@@ -1,5 +1,7 @@
 #Copy to Config.py. Replace < > content with your values. Remove this comment.
 config = {
+    "blackboard_domain" : "<Blackboard Learn server FQDN>",
+    "this_domain" : "<This app server FQDN>",
     "key" : "<Your REST Sey Here>",
     "secret" : "<Your REST Secret Here>",
     "url" : "<Your Learn Server URL Here. (No trailing/)>",
